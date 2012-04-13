6c1fe430c57b3a17b2e560d0965c5838b2e95263
Gaël PORT <gael.port@rbs.fr>
2012-04-13 15:58:05 +0200

    [NEW] Add getShortDescriptionAsHtml and hasLongDescription on shipping modes.

--- persistentdocument/mode.class.php
+++ persistentdocument/mode.class.php
@@ -13,4 +13,31 @@ class shipping_persistentdocument_mode extends shipping_persistentdocument_modeb
 	{
 		return $this->getLabelAsHtml();	
 	}
+	
+	/**
+	 * @param integer $maxCount
+	 * @return string
+	 */
+	public function getShortDescriptionAsHtml($maxCount = 80)
+	{
+		if ($this->hasLongDescription($maxCount))
+		{
+			$desc = f_util_HtmlUtils::htmlToText($this->getDescription(), false, true);
+			$desc = f_util_StringUtils::shortenString($desc, $maxCount);
+			return f_util_HtmlUtils::textToHtml($desc);
+		}
+		else
+		{
+			return $this->getDescription();
+		}
+	}
+	
+	/**
+	 * @param integer $maxCount
+	 * @return boolean
+	 */
+	public function hasLongDescription($maxCount = 80)
+	{
+		return f_util_StringUtils::strlen($this->getDescription()) > $maxCount;
+	}
 }
